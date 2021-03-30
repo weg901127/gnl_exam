78edd28571a10271f6e321f17e5b85ce67e9774c
weg901127 <gilee@student.42seoul.kr>
2021-03-30 20:41:21 +0900

    add malloc(in precess)

--- get_next_line.c
+++ get_next_line.c
@@ -1,24 +1,34 @@
-#define BUFFER_SIZE 1
+//#define BUFFER_SIZE 1
 #define ERR -1
 #define DONE 1
 #define END 0
 #include <limits.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include "get_next_line.h"
 
-char *ft_strchr(void *str, char c)
+char *ft_strchr(char *str, char c)
 {
-	while(*(char *)str != c || !(*(char *)str))
+	while(*str != c && *str != '\0')
 		str++;
 	return (str);
 }
+
+void ft_memcpy(char *d, char *s, int count)
+{
+	while (count--)
+		*d++ = *s++;
+}
+
 int	get_next_line(int fd, char **line)
 {
 	static char bk[1000000];//			= {0,};
 	static int	is_end_read;
 	static int	i;
 	static int	cur;
+	static char	*tmp;
 	int			k;
 	char 		buf[BUFFER_SIZE + 1]	= {0,};
 
@@ -40,20 +50,46 @@ int	get_next_line(int fd, char **line)
 				i++;
 			}
 		}
+		bk[i] = '\0';
+		tmp = bk;
 	}
-	printf("\ni = %d\n", i);
-	write(1, bk, i-1);
+	if (i > 0)
+	{
+		if (!*tmp)
+			tmp++;
+		cur = ft_strchr(tmp, '\n') - tmp;
+		*(tmp + cur) = '\0';
+		*line = (char *)malloc((cur + 1)*sizeof(char));
+		line[cur] = 0;
+		ft_memcpy(*line, tmp, cur + 1);
+		//*line = *line - cur;
+		tmp = tmp + cur;
+		i = i-cur-1;
+		return (DONE);
+	}
+	else
+	{
+		*line = 0;
+		return (END);
+	}
+	//write(1, bk, i);
 
 	//read --> until end (0)
 	//get_line --> set line
 	//set_next_line -->set 
-	return (0);
+	//return (0);
 }
 int main ()
 {
 	char *line;
 	int fd = open("test", O_RDWR);
-	get_next_line(fd, &line);
+
+	while (line != 0)
+	{
+		get_next_line(fd, &line);
+		printf("%s\n",line);
+		//free(line);
+	}
 	//printf("%s", line);
 	close(fd);
 	return (0);
---
+++ get_next_line.h
@@ -0,0 +1,3 @@
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 32
+#endif
